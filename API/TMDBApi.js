d5d27927ef677b704a6ee102a9197d0c5b8173d1
Ju <50602425+jumalley@users.noreply.github.com>
2019-05-14 07:24:23 +0200

    Add files via upload

---
+++ API/TMDBApi.js
@@ -0,0 +1,12 @@
+const API_TOKEN = '0d08a16bdf58bd4ab366e067a013ffb4'
+
+export function getFilmsFromApiWithSearchedText(text, page) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + '&page=' + page
+  return fetch(url)
+  .then((response) => response.json())
+  .catch((error) => console.error(error))
+}
+
+export function getImageFromApi(name) {
+  return 'https://image.tmdb.org/t/p/w300' + name
+}
